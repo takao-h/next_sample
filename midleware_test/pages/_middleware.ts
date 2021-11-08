d471db094ee55044ce46c4e98349cbc67daa7ae5
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-11-09 05:51:30 +0900

    added query params filter

--- midleware_test/pages/_middleware.ts
+++ midleware_test/pages/_middleware.ts
@@ -1,5 +1,19 @@
-import type {NextFetchEvent, NextRequest} from 'next/server';
+import {NextFetchEvent, NextRequest, NextResponse} from 'next/server';
+
+const allowedParams = ['hoge']
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  return new Response('Hello Middleware!!!')
+  const url = req.nextUrl
+  let changed = false
+
+  url.searchParams.forEach((_, key) => {
+    if (!allowedParams.includes(key)) {
+      url.searchParams.delete(key)
+      changed = true
+    }
+  })
+
+  if (changed) {
+    return NextResponse.redirect(url)
+  }
 }
