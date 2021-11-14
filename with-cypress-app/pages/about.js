eb82961622ac42fc75a558e359188759d28b14cc
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-11-14 20:39:23 +0900

    added test

--- with-cypress-app/pages/about.js
+++ with-cypress-app/pages/about.js
@@ -1,17 +1,11 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import About from './about';
 
 export default function About() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>About Page</h1>
-        <p className={styles.description}>
-          <Link href="/">
-            <a>&larr; Go Back</a>
-          </Link>
-        </p>
-      </main>
+    <div>
+      <h1>About Page</h1>
     </div>
   )
 }
